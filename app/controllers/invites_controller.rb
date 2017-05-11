247bc86e04c2de5383d3533b837116cc89e57903
Justin Nguyen <nguyen.binh.dieu@framgia.com>
2017-05-11 16:36:57 +0700

    Refactor css and add action controll on org show page

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,7 +2,6 @@ class InvitesController < ApplicationController
   before_action :check_invite_permission, only: :show
 
   def show
-    @organization = Organization.find_by id: params[:organization_id]
     @user_organization = UserOrganization.new
     @users = User.can_invite_to_organization params[:organization_id]
   end
@@ -10,8 +9,10 @@ class InvitesController < ApplicationController
   private
 
   def check_invite_permission
-    @user_organization = UserOrganization.find_by user_id: current_user.id,
-      organization_id: params[:organization_id]
+    @organization = Organization.find_by slug: params[:organization_id]
+    @user_organization = UserOrganization.find_by user: current_user,
+      organization: @organization
+
     if @user_organization.nil? || @user_organization.waiting?
       redirect_to organizations_path, notice: t(".no_permission")
     end
